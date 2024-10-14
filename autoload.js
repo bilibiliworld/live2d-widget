13b4e7f082741d58dd42990401df5708ecd4e11b
bilibiliworld <fulafulovecina@gmail.com>
2024-10-14 14:45:03 +0800

    update

--- autoload.js
+++ autoload.js
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "http://8.223.47.89:8002/live2d_api/",
+			apiPath: "https://apache.002026.xyz/live2d_api/",
 			// cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
